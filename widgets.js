eb521896a4617e1a2b6b40ccf93e1301148367c2
App Academy Student <appacademy@Apps-Mac-mini-36.local>
2015-11-09 12:16:23 -0500

    Finish weather and temp widget

--- widgets.js
+++ widgets.js
@@ -1,16 +1,15 @@
-
-  var AutoComplete = React.createClass({
+ var AutoComplete = React.createClass({
     getInitialState: function(){
       return {input: "", returnWords: this.parseNames()};
     },
 
-    updateInput: function(event) {
+  updateInput: function(event) {
       this.searchString = event.target.value;
       console.log(this.state.returnWords);
       this.setState({returnWords: this.parseNames(), input: event.target.value});
     },
 
-    autoCompleteInput: function(event) {
+   autoCompleteInput: function(event) {
       var text = event.target.textContent;
       this.setState({input: event.target.textContent});
     },
@@ -35,4 +34,84 @@
         </div>
       );
     }
+  });
+
+
+
+
+  var WeatherClock = React.createClass({
+    getInitialState: function() {
+      return { time: this.props.time }
+    },
+
+    componentDidMount: function() {
+      navigator.geolocation.getCurrentPosition(function(pos) {
+        this.lat = pos.coords.latitude;
+        this.lon = pos.coords.longitude;
+        this.requestFromAPI();
+      }.bind(this))
+      this.dateTimer = setInterval(function(){
+      this.setState({time: new Date()})
+      }.bind(this), 1000);
+    },
+
+    requestFromAPI: function loadXMLDoc() {
+      var xmlhttp;
+
+      if (window.XMLHttpRequest) {
+          // code for IE7+, Firefox, Chrome, Opera, Safari
+          xmlhttp = new XMLHttpRequest();
+      } else {
+          // code for IE6, IE5
+          xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+      }
+
+      xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+             if(xmlhttp.status == 200){
+               var response = xmlhttp.responseText;
+               this.parseWeather(response);
+             }
+             else if(xmlhttp.status == 400) {
+                alert('There was an error 400')
+             }
+             else {
+                 alert('something else other than 200 was returned')
+             }
+          }
+      }.bind(this);
+
+      var url = "http://api.openweathermap.org/data/2.5/weather?lat=";
+      url += (this.lat + "&lon=" + this.lon);
+      url += "&APPID=" + "5a872b96603a66459f4c0d41fa0d69a9";
+      console.log(url);
+
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send();
+  },
+
+  parseWeather: function(response){
+    var weather = JSON.parse(response);
+    var weatherString = weather.name + ": " + weather.weather[0].description
+    this.setState({currentWeather: weatherString})
+  },
+
+    componentWilUnmount: function() {
+      clearTimeout(this.dateTimer);
+    },
+    renderTime: function() {
+      return this.state.time.toString();
+    },
+
+    render: function() {
+
+      return (
+        <div>
+
+          <div>{ this.renderTime() }</div>
+          <div>{this.state.currentWeather}</div>
+        </div>
+      )
+    }
+
   });
